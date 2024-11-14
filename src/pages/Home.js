fc4e24824849e95651f87aef5ffeed0493f69168
Reagandenis <dennisowiti215@gmail.com>
2024-11-14 22:16:07 +0300

    build process

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -58,7 +58,7 @@ const Homepage = () => {
             <section className="bg-gray-200 py-16 px-8">
                 <h3 className="text-2xl font-bold text-center text-gray-800 mb-8">Top Creators</h3>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-                    {["Artist 1", "Artist 2", "Artist 3"].map((creator, index) => (
+                    {["Reagan Enoch O.", "Alice mark A.", "Jackson Mark E."].map((creator, index) => (
                         <div key={index} className="bg-white rounded-lg shadow-lg p-4 text-center">
                             <img src="/Original.png" alt={creator} className="w-24 h-24 rounded-full mx-auto mb-4"/>
                             <h4 className="text-xl font-semibold">{creator}</h4>
